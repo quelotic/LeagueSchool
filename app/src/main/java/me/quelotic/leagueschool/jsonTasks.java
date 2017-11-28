039d26afab5f36837bf3ead3f424f712bc2b2db6
Thomas Antoniou <thomas.antoniou@hotmail.com>
2017-11-28 15:05:12 +0200

    jsonTask class initial commit plus some small edits

---
+++ app/src/main/java/me/quelotic/leagueschool/jsonTasks.java
@@ -0,0 +1,71 @@
+package me.quelotic.leagueschool;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class jsonTasks extends AsyncTask<String, String, String> {
+
+
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+
+    private AsyncResponse delegate = null;
+
+    jsonTasks(AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        HttpsURLConnection connection = null;
+        BufferedReader reader = null;
+
+        String result = null;
+
+        try {
+            URL url = new URL(params[0]);
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.connect();
+
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder buffer = new StringBuilder();
+
+            String line;// = "";
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+
+            result = buffer.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.processFinish(result);
+    }
+}
--- app/src/main/java/me/quelotic/leagueschool/summonerProfile.java
+++ app/src/main/java/me/quelotic/leagueschool/summonerProfile.java
@@ -1,9 +1,7 @@
 package me.quelotic.leagueschool;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -17,15 +15,8 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-import javax.net.ssl.HttpsURLConnection;
 
 public class summonerProfile extends Activity {
 
@@ -42,18 +33,33 @@ public class summonerProfile extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_summoner_profile);
 
-        txtSumName = (TextView) findViewById(R.id.sumName);
-        txtSumLevel = (TextView) findViewById(R.id.sumLevel);
-        imgSumIcon = (ImageView) findViewById(R.id.sumIcon);
+        txtSumName = findViewById(R.id.sumName);
+        txtSumLevel = findViewById(R.id.sumLevel);
+        imgSumIcon = findViewById(R.id.sumIcon);
 
         Bundle bundle = getIntent().getExtras();
         String bunSumName = bundle.getString("sumName");
         final String bunServer = bundle.getString("server");
 
-        new summonerProfile.JSONTask().execute("https://" + bunServer + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + bunSumName + "?api_key=RGAPI-5f588f65-8e43-4503-8bdc-a29de1561ff6");
+        //new summonerProfile.JSONTask().execute("https://" + bunServer + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + bunSumName + "?api_key=RGAPI-ec80c1dd-568d-4425-a3a2-671716a9a011");
+
+
+
+        jsonTasks asyncTask = new jsonTasks(new jsonTasks.AsyncResponse() {
+
+            @Override
+            public void processFinish(String output) {
+                txtSumName.setText(output);
+            }
+        });
+
+        asyncTask.execute("https://" + bunServer + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + bunSumName + "?api_key=RGAPI-ec80c1dd-568d-4425-a3a2-671716a9a011");
+
+
+
 
         //Button brings up match history
-        Button btnMatchHistory = (Button)findViewById(R.id.btnMatchHistory);
+        Button btnMatchHistory = findViewById(R.id.btnMatchHistory);
         btnMatchHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,85 +79,85 @@ public class summonerProfile extends Activity {
         });
     }
 
-    public class JSONTask extends AsyncTask<String, String, ArrayList> {
-
-        ProgressDialog dialog;
-
-        @Override
-        protected void onPreExecute() {
-            dialog = new ProgressDialog(summonerProfile.this);
-            dialog.setTitle("Fetching data");
-            dialog.setMessage("Please wait...");
-            dialog.setIndeterminate(true);
-            dialog.show();
-        }
-
-        @Override
-        protected ArrayList doInBackground(String... params) {
-
-            HttpsURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpsURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                ArrayList result = new ArrayList();
-                String finalJSON = buffer.toString();
-                JSONObject sumInfo = null;
-                try {
-                    sumInfo = new JSONObject(finalJSON);
-                    result.add(sumInfo.getString("name"));
-                    result.add(Integer.toString(sumInfo.getInt("summonerLevel")));
-                    result.add(Integer.toString(sumInfo.getInt("profileIconId")));
-                    result.add(Integer.toString(sumInfo.getInt("id")));
-                    result.add(Integer.toString(sumInfo.getInt("accountId")));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                return result;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList result) {
-            super.onPostExecute(result);
-            if (result != null) {
-                txtSumName.setText((String) result.get(0));
-                txtSumLevel.setText((String) result.get(1));
-                Picasso.with(getBaseContext()).load("https://ddragon.leagueoflegends.com/cdn/7.23.1/img/profileicon/" + result.get(2) + ".png").into(imgSumIcon);
-                forBunSumID = (String) result.get(3);
-                forBunAccID = (String) result.get(4);
-            }
-            dialog.dismiss();
-        }
-    }
+//    public class JSONTask extends AsyncTask<String, String, ArrayList> {
+//
+//        ProgressDialog dialog;
+//
+//        @Override
+//        protected void onPreExecute() {
+//            dialog = new ProgressDialog(summonerProfile.this);
+//            dialog.setTitle("Fetching data");
+//            dialog.setMessage("Please wait...");
+//            dialog.setIndeterminate(true);
+//            dialog.show();
+//        }
+//
+//        @Override
+//        protected ArrayList doInBackground(String... params) {
+//
+//            HttpsURLConnection connection = null;
+//            BufferedReader reader = null;
+//
+//            try {
+//                URL url = new URL(params[0]);
+//                connection = (HttpsURLConnection) url.openConnection();
+//                connection.connect();
+//
+//                InputStream stream = connection.getInputStream();
+//                reader = new BufferedReader(new InputStreamReader(stream));
+//                StringBuffer buffer = new StringBuffer();
+//
+//                String line = "";
+//                while ((line = reader.readLine()) != null) {
+//                    buffer.append(line);
+//                }
+//
+//                ArrayList result = new ArrayList();
+//                String finalJSON = buffer.toString();
+//                JSONObject sumInfo = null;
+//                try {
+//                    sumInfo = new JSONObject(finalJSON);
+//                    result.add(sumInfo.getString("name"));
+//                    result.add(Integer.toString(sumInfo.getInt("summonerLevel")));
+//                    result.add(Integer.toString(sumInfo.getInt("profileIconId")));
+//                    result.add(Integer.toString(sumInfo.getInt("id")));
+//                    result.add(Integer.toString(sumInfo.getInt("accountId")));
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//                return result;
+//
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (connection != null) {
+//                    connection.disconnect();
+//                }
+//                try {
+//                    if (reader != null) {
+//                        reader.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(ArrayList result) {
+//            super.onPostExecute(result);
+//            if (result != null) {
+//                txtSumName.setText((String) result.get(0));
+//                txtSumLevel.setText((String) result.get(1));
+//                Picasso.with(getBaseContext()).load("https://ddragon.leagueoflegends.com/cdn/7.23.1/img/profileicon/" + result.get(2) + ".png").into(imgSumIcon);
+//                forBunSumID = (String) result.get(3);
+//                forBunAccID = (String) result.get(4);
+//            }
+//            dialog.dismiss();
+//        }
+//    }
 }
